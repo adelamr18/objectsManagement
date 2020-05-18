ec95ab408297b1c19bd277435de5687b434e570e
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-18 12:41:35 +0200

    Create defines file to hold application static text and constants

---
+++ src/shared/constants/defines.tsx
@@ -0,0 +1,29 @@
+export const titles = {
+  objectsHeader: "Objects",
+  confirm: "Confirm",
+  add: "Add"
+};
+
+export const modalTitles = {
+  confirm: "Confirm",
+  add: "Add",
+  close: "Close",
+  editObject: "Edit object",
+  addObject: "Add object",
+  description: "Description",
+  name: "Name",
+  relations: "Relations",
+  type: "Type"
+};
+
+export const searchBarTitles = {
+  placeholder: "Search for an object"
+};
+
+export const relationsTitles = {
+  desk: "desk",
+  employee: "employee",
+  calculator: "calculator",
+  keyboard: "keyboard",
+  server: "server"
+};
