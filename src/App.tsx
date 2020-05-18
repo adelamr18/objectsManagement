84638476ad5e21e48f8e85da0c7568778612b068
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-18 12:46:21 +0200

    Use header component in application

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ObjectsDashboard from "./views/objects-dashboard/objects-dashboard";
+import Header from "./shared/components/header/header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="header-container-outer">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
