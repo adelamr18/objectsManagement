4402ac0ae81c6601acce21366bbd876bd89cc47d
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-18 12:39:43 +0200

    Configure application to use redux persist and connect store

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import topLevelReducers from "./reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import reduxThunk from "redux-thunk";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["objects"]
+};
+const persistedReducer = persistReducer(persistConfig, topLevelReducers);
+const store = createStore(
+  persistedReducer,
+  {},
+  compose(applyMiddleware(reduxThunk, thunk))
+);
+const persistor = persistStore(store);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
